332997de5c57d2d7336be10c3816e295929e1ba7
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-17 15:27:02 -0500

    Added command prompt source and header files.

---
+++ include/commands.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __commands_h__
+#define __commands_h__
+
+#define PROMPT_CLOSE 0
+#define PROMPT_OPEN 1
+#endif // !__commands_h__
---
+++ src/commands.c
@@ -0,0 +1,12 @@
+/*
+* Name: Chase Counsell
+* Project: COMP 5400 Project 4
+* File: commands.c
+* Date: 4/17/2016
+* Description: Functions for creating text commands
+*/
+
+void initialize_command_prompt(void)
+{
+
+}
