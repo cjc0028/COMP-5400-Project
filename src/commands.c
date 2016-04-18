a3fb22786c33bd4c7bcbfc8fc97065cab3f0d7af
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-18 08:49:28 -0500

    Console handles backspace and mid-string typing. Added cursor position symbol.

--- src/commands.c
+++ src/commands.c
@@ -8,12 +8,18 @@
 
 
 #include<stdlib.h>
+#include<math.h>
 #include<GL/glut.h>
 #include "commands.h"
-
+#include "person.h"
+#include "camera.h"
+#include "buildings.h"
+#include "lighting.h"
 
 #define BUFFER_SIZE 100
+
 static int prompt_state = PROMPT_CLOSE;
+static char* modules[] = {"person", "buildings", "camera", "lighting"};
 
 typedef struct Buffer
 {
@@ -22,10 +28,14 @@ typedef struct Buffer
 }Buffer;
 
 static Buffer buffer;
+GLint cursor_pos[2];
+static GLint cursor_offset = 0;
+static int selected_buffer_index = 0;
 
 void open_command_prompt(void)
 {
 	prompt_state = PROMPT_OPEN;
+	cursor_offset = 0.0;
 }
 
 void close_command_prompt(void)
@@ -64,11 +74,36 @@ void draw_prompt(void)
 	glEnd();
 
 	glColor3f(0.0, 0.0, 0.0);
+	glRasterPos2i(2.0, 5.0);
 	for (int i = 0; i < buffer.length; i++)
 	{
-		glRasterPos2f(2.0 + (i * 7), 4.0);
-		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, buffer.string[i]);
+		if (i == selected_buffer_index)
+		{
+			//glPushMatrix();
+			glColor3f(1.0, 1.0, 1.0);
+			glutBitmapCharacter(GLUT_BITMAP_9_BY_15, buffer.string[i]);
+			//glPopMatrix();
+		}
+		else
+		{
+			//glPushMatrix();
+			glColor3f(0.0, 0.0, 0.0);
+			glutBitmapCharacter(GLUT_BITMAP_9_BY_15, buffer.string[i]);
+			//glPopMatrix();
+		}
+
+		glRasterPos2i(2.0 + ((i+1) * 9), 4.0);
 	}
+
+	glColor3f(0.0, 0.0, 0.0);
+	glGetIntegerv(GL_CURRENT_RASTER_POSITION, cursor_pos);
+	cursor_pos[0] += (9 * cursor_offset);
+	glBegin(GL_POLYGON);
+		glVertex2f(cursor_pos[0], cursor_pos[1] - 4);
+		glVertex2f(cursor_pos[0] + 9, cursor_pos[1] - 4);
+		glVertex2f(cursor_pos[0] + 9, cursor_pos[1] + 9);
+		glVertex2f(cursor_pos[0], cursor_pos[1] + 9);
+	glEnd();
 
 	glPopMatrix();
 	glMatrixMode(GL_PROJECTION);
@@ -81,11 +116,48 @@ void write_to_prompt(unsigned char c)
 {
 	if (buffer.length > 0 && c == 8)
 	{
-		buffer.string[--buffer.length] = '0';
+		buffer.string[selected_buffer_index] = '0';
+		buffer.length--;
+		for (int i = selected_buffer_index; i < buffer.length; i++)
+		{
+			buffer.string[i] = buffer.string[i + 1];
+		}
+
+		//buffer.string[buffer.length] = '0';
 	}
 	else if (c != 8)
 	{
 		if (buffer.length < BUFFER_SIZE)
-			buffer.string[buffer.length++] = c;
+		{
+			buffer.length++;
+			for (int i = buffer.length; i > selected_buffer_index; i--)
+			{
+				//char temp = buffer.string[i + 1];
+				buffer.string[i] = buffer.string[i - 1];
+			}
+			buffer.string[selected_buffer_index] = c;
+		}
+	}
+
+	selected_buffer_index = buffer.length + cursor_offset;
+}
+
+void move_cursor(int key)
+{
+	if (key == GLUT_KEY_LEFT && abs(cursor_offset) < buffer.length) cursor_offset -= 1;
+	else if (key == GLUT_KEY_RIGHT && cursor_offset < 0) cursor_offset += 1;
+
+	selected_buffer_index = buffer.length + cursor_offset;
+}
+
+void run_command(void)
+{
+	if (buffer.string[0] != '/')
+	{
+
+	}
+	else
+	{
+
 	}
 }
