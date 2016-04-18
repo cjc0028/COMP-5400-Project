8bf65f9c5a8fb2a9372457b375268929865dbe28
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-18 13:11:58 -0500

    Added "get_buildings" function definition.

--- include/buildings.h
+++ include/buildings.h
@@ -6,4 +6,5 @@ void draw_buildings(void);
 void calculate_bounds(struct Building *building, GLfloat deltaX, GLfloat deltaY, GLfloat deltaZ, GLfloat scaleX, GLfloat scaleY, GLfloat scaleZ);
 void draw_bounds(void);
 GLfloat * get_ground_size(void);
+GLfloat * get_buildings(void);
 #endif // !__buildings_h__
