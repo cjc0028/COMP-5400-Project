898a25dc14a8c67db9f7717049a34aa8287c9a8d
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-18 19:26:43 -0500

    Generalized "student" functions to any person. Added additional functions.

--- include/person.h
+++ include/person.h
@@ -2,13 +2,28 @@
 #ifndef __person_h__
 #define __person_h__
 
-void draw_student(void);
-void draw_bystander(void);
-void rotate_student(GLfloat phi, GLfloat psi);
-void translate_student(GLfloat delta);
-void scale_student(GLfloat factor);
+typedef struct Person
+{
+	int is_student;
+	GLfloat position[4];                                               // X, Y, and Z position
+	GLfloat scale;                                                     // Scale used in all directions
+	GLfloat angle[3];                                                  // [0] - body tilt, [1] - body rotation
+	GLfloat head_angle[3];
+	int colors[4];
+} Person;
+
+void create_student(int skin_color, int shirt_color, int pants_color, int shoe_color);
+void create_bystander(int skin_color, int shirt_color, int pants_color, int shoe_color);
+
+void draw_person(void);
+void rotate_person(GLfloat phi, GLfloat psi);
+void translate_person(GLfloat delta);
+void scale_person(GLfloat factor);
 void rotate_head(GLfloat phi, GLfloat psi);
 GLfloat * get_position(void);
-GLfloat * get_angle(void);
-void reset_student(void);
+Person * get_people(void);
+void select_person(int person);
+int get_selected_person(void);
+int get_num_people(void);
+void reset_person(void);
 #endif // !__person_h__
