34e2ffb9f2c9ff3a31da05e3c05891a903dbcc13
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-22 21:20:27 -0500

    Fixed function to only accept bounds as parameter.

--- src/collision.c
+++ src/collision.c
@@ -7,11 +7,12 @@
 */
 
 #include<GL/glut.h>
+#include<stdio.h>
 #include<math.h>
 #include "buildings.h"
 #include "collisions.h"
 
-int collision_detected(GLfloat * position, GLfloat * bounds)
+int collision_detected(GLfloat * bounds)
 {
 	int collision_x = 0, collision_y = 0, collision_z = 0;
 	int num_buildings = get_num_buildings();
@@ -20,9 +21,9 @@ int collision_detected(GLfloat * position, GLfloat * bounds)
 	{
 		for (int j = 0; j < building->num_bounds; j++)
 		{
-			collision_x = (abs(position[0] - building->bounds[j][0]) < bounds[3] + building->bounds[j][1]);
-			collision_y = (abs(position[1] - building->bounds[j][4]) < bounds[4] + building->bounds[j][5]);
-			collision_z = (abs(position[2] - building->bounds[j][2]) < bounds[5] + building->bounds[j][3]);
+			collision_x = (fabs(bounds[0] - building->bounds[j][0]) < bounds[1] + building->bounds[j][1]);
+			collision_y = (fabs(bounds[4] - building->bounds[j][4]) < bounds[5] + building->bounds[j][5]);
+			collision_z = (fabs(bounds[2] - building->bounds[j][2]) < bounds[3] + building->bounds[j][3]);
 			
 			if (collision_x && collision_y && collision_z) return 1;
 		}
