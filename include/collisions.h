706bd0bcee47b9e847ee25d6f9ca14260f3d35d2
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-22 21:15:05 -0500

    Removed parameter from collision_detected function.

--- include/collisions.h
+++ include/collisions.h
@@ -2,5 +2,5 @@
 #ifndef __collisions_h__
 #define __collisions_h__
 
-int collision_detected(GLfloat * position, GLfloat * bounds);
+int collision_detected(GLfloat * bounds);
 #endif // !__collisions_h__
