63414f026a04f397d0da3b75d0abeb4d8b35495d
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-28 17:13:25 -0500

    Added more command prompt functions

--- include/command_prompt.h
+++ include/command_prompt.h
@@ -8,7 +8,11 @@
 int get_prompt_state(void);
 void open_command_prompt(void);
 void close_command_prompt(void);
+void clear_prompt(void);
+void clear_history(void);
 void draw_prompt(void);
 void write_to_prompt(unsigned char c);
+void write_to_command_history(char * text);
 void move_cursor(int key);
+void run_command(void);
 #endif // !__commands_h__
