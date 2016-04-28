c78235e39318491c3d0d1e1085153eedef326de8
Chase J. Counsell <cjc0028@auburn.edu>
2016-04-28 17:19:55 -0500

    Added additional functions.

--- include/person.h
+++ include/person.h
@@ -11,7 +11,11 @@ typedef struct Body_Part
 typedef struct Person
 {
 	int is_student;
+	int in_air;
+	int speed;
+	int rotation_speed;
 	GLfloat position[4];                                               // X, Y, and Z position
+	GLfloat velocity[4];											   // Player velocity
 	GLfloat scale;                                                     // Scale used in all directions
 	GLfloat angle[3];                                                  // [0] - body tilt, [1] - body rotation
 	int colors[4];
@@ -19,18 +23,28 @@ typedef struct Person
 	GLfloat bounds[6];
 } Person;
 
-void create_student(int skin_color, int shirt_color, int pants_color, int shoe_color);
-void create_bystander(int skin_color, int shirt_color, int pants_color, int shoe_color);
+void initialize_people(void);
 
-void draw_person(void);
+void create_student(int num_args, ...);
+void create_bystander(int num_args, ...);
+
+void draw_person(int person);
 void rotate_person(GLfloat phi, GLfloat psi);
 void translate_person(GLfloat delta);
 void scale_person(GLfloat factor);
+void jump(int person);
 void rotate_head(GLfloat phi, GLfloat psi);
+void set_person_position(int person, int num_args, ...);
+void set_person_angle(int person, int num_args, ...);
+void set_person_scale(int person, GLfloat scale);
 GLfloat * get_position(void);
 Person * get_people(void);
+Person get_person(int person);
+GLfloat * get_bounds(void);
 void select_person(int person);
 int get_selected_person(void);
 int get_num_people(void);
 void reset_person(void);
+
+void person_commands(int num_args, char * args[]);
 #endif // !__person_h__
